58746cb8a111c39ceb5b54d0cf14f7b13ed9d8b0
Fernando Gasperi Jabalera <t-fegas@microsoft.com>
2018-03-21 15:56:15 -0700

    Created files for the Cmdlet and Model.

---
+++ src/ResourceManager/FileSync/Commands.FileSync.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Commands.FileSync.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Commands.FileSync.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ab2bef44-60dc-4e13-afdd-1453c2d5ccf1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
